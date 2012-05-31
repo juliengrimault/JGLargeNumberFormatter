b09e02264ffd19b64799e5a646b5a9ec922de78d
Julien Grimault <julien.grimault@me.com>
2012-05-31 22:39:57 +0800

    Handling of negative number.

--- JGLargeNumberFormatter/JGLargeNumberFormatter.m
+++ JGLargeNumberFormatter/JGLargeNumberFormatter.m
@@ -24,17 +24,18 @@
   }
   
   double d = [number doubleValue];
-  if ( self.billionsAbreviation && d >= 1000000000 )
+  double absolute = fabs(d);
+  if ( self.billionsAbreviation && absolute >= 1000000000 )
   {
     NSString* numberInBillion = [super stringFromNumber:[NSNumber numberWithDouble:d / 1000000000]];
     return [NSString stringWithFormat:@"%@%@", numberInBillion, self.billionsAbreviation];
   }
-  if ( self.millionsAbreviation && d >= 1000000 )
+  if ( self.millionsAbreviation && absolute >= 1000000 )
   {
     NSString* numberInMillion = [super stringFromNumber:[NSNumber numberWithDouble:d / 1000000]];
     return [NSString stringWithFormat:@"%@%@", numberInMillion, self.millionsAbreviation];
   }
-  if ( self.thousandsAbreviation && d >= 1000 )
+  if ( self.thousandsAbreviation && absolute >= 1000 )
   {
     NSString* numberInThousand = [super stringFromNumber:[NSNumber numberWithDouble:d / 1000]];
     return [NSString stringWithFormat:@"%@%@", numberInThousand, self.thousandsAbreviation];
